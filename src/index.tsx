eef57547629bd818a566cd595c8450ea766c7196
srodrigo28 <rodrigoexer1@gmail.com>
2022-03-04 06:22:18 -0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Dasboard from './pages/Dasboard';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dasboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/List/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List: React.FC = () =>{
+    return(
+        <>
+            <h1>Lista</h1>
+        </>
+    )
+}
+
+export default List;
